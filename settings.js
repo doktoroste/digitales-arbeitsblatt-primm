8894fbd7192dad2203c3cc15c4f5d960f2f09d03
doktoroste <24372854+doktoroste@users.noreply.github.com>
2025-04-14 15:57:31 +0200

    Format with Prettier

--- settings.js
+++ settings.js
@@ -2,23 +2,22 @@
 // -------------------------------------
 
 // If set to true, each exercise will ask for feedback after it is completed
-const askForFeedback = true
-
+const askForFeedback = true;
 
 // Layout
 // -------------------------------------
 
 // Programming language (see src/highlight/languages))
-const language = 'python'
+const language = "python";
 
 // Code snippet editor theme (see src/highlight/styles)
-const theme = 'github'
-const showLineNumbers = true
+const theme = "github";
+const showLineNumbers = true;
 const languageNames = {
-    "python": "Python",
-    "javascript": "JavaScript",
-    "html": "HTML",
-    "css": "CSS",
-    "bash": "Bash",
-    "plaintext": "Text"
-}
+  python: "Python",
+  javascript: "JavaScript",
+  html: "HTML",
+  css: "CSS",
+  bash: "Bash",
+  plaintext: "Text",
+};
